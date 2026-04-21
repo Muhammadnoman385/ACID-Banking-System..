07c05213af02b638045b23f790de79a67a116c69
HP 640 G5 <HP 640 G5@DESKTOP-OC1B5G3>
2026-04-21 21:20:16 +0500

    Add project files.

---
+++ ACID/Form1.Designer.cs
@@ -0,0 +1,205 @@
+﻿namespace ACID
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtNomanBalance = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtClientBalance = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtAmount = new System.Windows.Forms.TextBox();
+            this.btnSuccess = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.dgvHistory = new System.Windows.Forms.DataGridView();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvHistory)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(61, 92);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(122, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Noman Balance";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // txtNomanBalance
+            // 
+            this.txtNomanBalance.Location = new System.Drawing.Point(216, 86);
+            this.txtNomanBalance.Name = "txtNomanBalance";
+            this.txtNomanBalance.ReadOnly = true;
+            this.txtNomanBalance.Size = new System.Drawing.Size(189, 26);
+            this.txtNomanBalance.TabIndex = 1;
+            this.txtNomanBalance.TextChanged += new System.EventHandler(this.txtNomanBalance_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(72, 149);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(111, 20);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Client Balance";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // txtClientBalance
+            // 
+            this.txtClientBalance.Location = new System.Drawing.Point(216, 146);
+            this.txtClientBalance.Name = "txtClientBalance";
+            this.txtClientBalance.ReadOnly = true;
+            this.txtClientBalance.Size = new System.Drawing.Size(189, 26);
+            this.txtClientBalance.TabIndex = 3;
+            this.txtClientBalance.TextChanged += new System.EventHandler(this.txtClientBalance_TextChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(37, 210);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(146, 20);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Amount to Transfer";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // txtAmount
+            // 
+            this.txtAmount.Location = new System.Drawing.Point(216, 207);
+            this.txtAmount.Name = "txtAmount";
+            this.txtAmount.Size = new System.Drawing.Size(189, 26);
+            this.txtAmount.TabIndex = 5;
+            this.txtAmount.TextChanged += new System.EventHandler(this.txtAmount_TextChanged);
+            // 
+            // btnSuccess
+            // 
+            this.btnSuccess.BackColor = System.Drawing.Color.LimeGreen;
+            this.btnSuccess.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSuccess.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnSuccess.Location = new System.Drawing.Point(341, 290);
+            this.btnSuccess.Name = "btnSuccess";
+            this.btnSuccess.Size = new System.Drawing.Size(330, 48);
+            this.btnSuccess.TabIndex = 6;
+            this.btnSuccess.Text = "\"Transfer (Success Case)\"";
+            this.btnSuccess.UseVisualStyleBackColor = false;
+            this.btnSuccess.Click += new System.EventHandler(this.btnSuccess_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Crimson;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.button2.Location = new System.Drawing.Point(724, 290);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(325, 59);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "\"Transfer (Fail Case/Crash)\"";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // dgvHistory
+            // 
+            this.dgvHistory.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dgvHistory.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgvHistory.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvHistory.Location = new System.Drawing.Point(50, 25);
+            this.dgvHistory.Name = "dgvHistory";
+            this.dgvHistory.RowHeadersWidth = 62;
+            this.dgvHistory.RowTemplate.Height = 28;
+            this.dgvHistory.Size = new System.Drawing.Size(1122, 217);
+            this.dgvHistory.TabIndex = 8;
+            this.dgvHistory.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvHistory_CellContentClick);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.txtAmount);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.txtNomanBalance);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.txtClientBalance);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Location = new System.Drawing.Point(451, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(458, 272);
+            this.groupBox1.TabIndex = 9;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Account Details";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.dgvHistory);
+            this.groupBox2.Location = new System.Drawing.Point(98, 355);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(1201, 271);
+            this.groupBox2.TabIndex = 10;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Transaction History";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1352, 674);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.btnSuccess);
+            this.Name = "Form1";
+            this.Text = "ACID Banking System";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvHistory)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtNomanBalance;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtClientBalance;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtAmount;
+        private System.Windows.Forms.Button btnSuccess;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.DataGridView dgvHistory;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+    }
+}
+
---
+++ ACID/Form1.cs
@@ -0,0 +1,257 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.Data.Sqlite;
+
+namespace ACID
+{
+    public partial class Form1 : Form
+    {
+        // Connection string ko class level par rakha hai taake har jagah use ho sakay
+        string connString = "Data Source=AcidBank.db";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // SQLite ko initialize karna zaroori hai
+            SQLitePCL.Batteries.Init();
+            try
+            {
+                using (var conn = new SqliteConnection(connString))
+                {
+                    conn.Open();
+                    var cmd = conn.CreateCommand();
+
+                    // Portfolio ke liye do tables: Accounts aur TransactionHistory
+                    cmd.CommandText = @"
+                        CREATE TABLE IF NOT EXISTS Accounts (
+                            Id INTEGER PRIMARY KEY,
+                            Name TEXT,
+                            Balance INTEGER CHECK(Balance >= 0)
+                        );
+                        CREATE TABLE IF NOT EXISTS TransactionHistory (
+                            Id INTEGER PRIMARY KEY AUTOINCREMENT,
+                            Sender TEXT,
+                            Receiver TEXT,
+                            Amount INTEGER,
+                            Status TEXT,
+                            Timestamp DATETIME
+                        );
+                        INSERT OR IGNORE INTO Accounts (Id, Name, Balance) VALUES (1, 'Noman', 10000), (2, 'Client', 5000);";
+                    cmd.ExecuteNonQuery();
+                }
+                RefreshBalances();
+                LoadHistory(); // App start hotay hi purani history dikhayein
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Database Error: " + ex.Message);
+            }
+        }
+
+        private void RefreshBalances()
+        {
+            try
+            {
+                using (var conn = new SqliteConnection(connString))
+                {
+                    conn.Open();
+                    var cmd = conn.CreateCommand();
+                    cmd.CommandText = "SELECT Name, Balance FROM Accounts ORDER BY Id";
+
+                    using (var reader = cmd.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            string name = reader["Name"].ToString();
+                            string balance = reader["Balance"].ToString();
+
+                            if (name == "Noman") txtNomanBalance.Text = balance;
+                            if (name == "Client") txtClientBalance.Text = balance;
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Refresh Error: " + ex.Message);
+            }
+        }
+
+        // GridView mein history dikhane ka function
+        private void LoadHistory()
+        {
+            try
+            {
+                using (var conn = new SqliteConnection(connString))
+                {
+                    conn.Open();
+                    var cmd = conn.CreateCommand();
+                    // Sabse naye records uupar dikhane ke liye ORDER BY Id DESC
+                    cmd.CommandText = "SELECT Id, Sender, Receiver, Amount, Status, Timestamp FROM TransactionHistory ORDER BY Id DESC";
+
+                    DataTable dt = new DataTable();
+                    using (var reader = cmd.ExecuteReader())
+                    {
+                        dt.Load(reader);
+                    }
+
+                    // Grid mein data load karna
+                    dgvHistory.DataSource = dt;
+
+                    // --- Professional Portfolio Styling ---
+                    if (dgvHistory.Columns.Count > 0)
+                    {
+                        // Columns ke naam behtar karein
+                        dgvHistory.Columns["Sender"].HeaderText = "Bhejne Wala";
+                        dgvHistory.Columns["Receiver"].HeaderText = "Wasool Karne Wala";
+                        dgvHistory.Columns["Amount"].HeaderText = "Raqam";
+                        dgvHistory.Columns["Status"].HeaderText = "Halat";
+                        dgvHistory.Columns["Timestamp"].HeaderText = "Waqt";
+
+                        // Alignment aur Readability
+                        dgvHistory.Columns["Amount"].DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleRight;
+                        dgvHistory.AlternatingRowsDefaultCellStyle.BackColor = Color.FromArgb(235, 245, 255); // Halki blue line
+                        dgvHistory.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+                        dgvHistory.ReadOnly = true; // User grid mein change na kar sakay
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("History Load Error: " + ex.Message);
+            }
+        }
+
+        // --- SUCCESS CASE ---
+        private void btnSuccess_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(txtAmount.Text)) return;
+            if (!int.TryParse(txtAmount.Text, out int amount)) return;
+
+            using (var conn = new SqliteConnection(connString))
+            {
+                conn.Open();
+                using (var transaction = conn.BeginTransaction())
+                {
+                    try
+                    {
+                        // Step 1: Noman se minus
+                        var cmd1 = conn.CreateCommand();
+                        cmd1.Transaction = transaction;
+                        cmd1.CommandText = $"UPDATE Accounts SET Balance = Balance - {amount} WHERE Name = 'Noman'";
+                        cmd1.ExecuteNonQuery();
+
+                        // Step 2: Client mein jama
+                        var cmd2 = conn.CreateCommand();
+                        cmd2.Transaction = transaction;
+                        cmd2.CommandText = $"UPDATE Accounts SET Balance = Balance + {amount} WHERE Name = 'Client'";
+                        cmd2.ExecuteNonQuery();
+
+                        // Step 3: History mein 'SUCCESS' record karein
+                        var cmdLog = conn.CreateCommand();
+                        cmdLog.Transaction = transaction;
+                        cmdLog.CommandText = "INSERT INTO TransactionHistory (Sender, Receiver, Amount, Status, Timestamp) " +
+                                             "VALUES ('Noman', 'Client', @amt, 'SUCCESS', DATETIME('now'))";
+                        cmdLog.Parameters.AddWithValue("@amt", amount);
+                        cmdLog.ExecuteNonQuery();
+
+                        transaction.Commit();
+                        MessageBox.Show("Mubarak ho! Transaction Successful.");
+                    }
+                    catch (Exception ex)
+                    {
+                        transaction.Rollback();
+                        MessageBox.Show("Transaction Fail: " + ex.Message);
+                    }
+                }
+            }
+            RefreshBalances();
+            LoadHistory();
+        }
+
+        // --- CRASH / FAIL CASE ---
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(txtAmount.Text)) return;
+            if (!int.TryParse(txtAmount.Text, out int amount)) return;
+
+            using (var conn = new SqliteConnection(connString))
+            {
+                conn.Open();
+                using (var transaction = conn.BeginTransaction())
+                {
+                    try
+                    {
+                        // 1. Noman ke paise katne ki koshish
+                        var cmd1 = conn.CreateCommand();
+                        cmd1.Transaction = transaction;
+                        cmd1.CommandText = $"UPDATE Accounts SET Balance = Balance - {amount} WHERE Name = 'Noman'";
+                        cmd1.ExecuteNonQuery();
+
+                        // 2. Zabardasti Crash Simulation
+                        throw new Exception("System Crash Simulation!");
+
+                        transaction.Commit();
+                    }
+                    catch (Exception ex)
+                    {
+                        // ACID: Rollback sab kuch purani halat par le aayega
+                        transaction.Rollback();
+                        MessageBox.Show("Crash Hua! Rollback ki wajah se balance aur history dono bach gaye.");
+                    }
+                }
+            }
+            RefreshBalances();
+            LoadHistory();
+        }
+
+        private void dgvHistory_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            dgvHistory.Columns["Sender"].HeaderText = "Bhejne Wala";
+            dgvHistory.Columns["Receiver"].HeaderText = "Wasool Karne Wala";
+            dgvHistory.Columns["Amount"].HeaderText = "Raqam";
+            dgvHistory.Columns["Status"].HeaderText = "Halat";
+        }
+
+        private void txtAmount_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtClientBalance_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtNomanBalance_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
